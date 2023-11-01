fbaec75a98d15fb6b2d50071de29f871f7a36bac
murodovi005 <146916092+murodovi005@users.noreply.github.com>
2023-11-01 11:05:36 +0500

    Update My cods.py

--- My cods.py
+++ My cods.py
@@ -1,43 +1,47 @@
-while True:
-    # ваш код, который нужно повторять
-    password = input("Your password please")
-    if password == "112233":
-        print("Welcome")
-    else:
-        print("Wrong !")
-    # проверяем условие для выхода из бесконечного цикла (например, при вводе "q")
-    if input("Хотите выйти? (y/n): ") == "y":
-        break  # выходим из цикла
+def project1():
+    while True:
+        # ваш код, который нужно повторять
+        password = input("Your password please")
+        if password == "112233":
+            print("Welcome")
+        else:
+            print("Wrong !")
+        # проверяем условие для выхода из бесконечного цикла (например, при вводе "q")
+        if input("Хотите выйти? (y/n): ") == "y":
+            break  # выходим из цикла
+project1()
 
 
-    
+def project2():
     import tkinter as tk
-from PIL import Image, ImageTk
+    from PIL import Image, ImageTk
 
-# Создание главного окна
-window = tk.Tk()
+    # Создание главного окна
+    window = tk.Tk()
 
-# Загрузка изображения
-image = Image.open("example.jpg")
-image = image.resize((400, 300))  # Изменение размера изображения
+    # Загрузка изображения
+    image = Image.open("example.jpg")
+    image = image.resize((400, 300))  # Изменение размера изображения
 
-# Конвертация изображения для использования в Tkinter
-tk_image = ImageTk.PhotoImage(image)
+    # Конвертация изображения для использования в Tkinter
+    tk_image = ImageTk.PhotoImage(image)
 
-# Создание виджета для отображения изображения
-image_label = tk.Label(window, image=tk_image)
-image_label.pack()
+    # Создание виджета для отображения изображения
+    image_label = tk.Label(window, image=tk_image)
+    image_label.pack()
 
-# Запуск главного цикла окна
-window.mainloop()
+    # Запуск главного цикла окна
+    window.mainloop()
+project2()
 
 
-#2
-import time
+def project3():
+    import time
 
-while True:
-    # Ваш код здесь
-    print("Привет, мир!")
+    while True:
+        # Ваш код здесь
+        print("Привет, мир!")
 
-    # Создайте задержку в 1 секунду перед следующей итерацией
-    time.sleep(5)
+        # Создайте задержку в 1 секунду перед следующей итерацией
+        time.sleep(5)
+project3()
