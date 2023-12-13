df793188387c7b11047b6335cff8cbaf64dd0950
murodovi005 <146916092+murodovi005@users.noreply.github.com>
2023-12-13 11:13:35 +0500

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -6,3 +6,42 @@ def task1():
         print('Нечентное число')
 
 # task1()
+
+
+def task1():
+    def numb(b):
+        '''1.Определение чётности чисел'''
+
+        if b % 2 == 0:
+            return True
+        else:
+            return False
+
+    # 2.Определени палиндром
+    def palindrom(a):
+        a3 = a % 10
+        a2_1 = (a - a3)
+        a2 = (a2_1 / 10) % 10
+        a1 = (a2_1 / 10 - a2) / 10
+        print(a1, a2, a3)
+        if a1 == a3:
+            return True
+        else:
+            return False
+
+    x = input()
+    palindrom(x)
+    numb(x)
+    if palindrom(x) == True:
+        print("Палиндром")
+    else:
+        print("не Палиндром")
+
+    if numb(x) == True:
+        print("Чётное число ")
+    else:
+        print("Не чётное число ")
+
+
+task1()
+
